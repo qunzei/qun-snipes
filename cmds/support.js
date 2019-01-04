33456f258a1fb2d68e227841af57212de06a427e
Mason Berndt <qunzei@masons-mbp.attlocal.net>
2019-01-04 14:55:41 -0600

    Handle login rejection

---
+++ cmds/support.js
@@ -0,0 +1,9 @@
+
+module.exports.run = async (bot,message,args) => {
+    message.channel.send("You can support the creator of this bot, (Qunzei#1932) by donating whatever amount you would like to at https://paypal.me/qunzei");
+}
+
+
+module.exports.help = {
+    name: "support"
+}
